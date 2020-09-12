6782345c7fd005474dc1d6265c8d036e9ee2655d
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-09-13 00:07:57 +0530

    Add React files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
