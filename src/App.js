a57131beb574bbd98822ac340fc467365fb2ddc7
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-09-13 00:43:13 +0530

    Add Header Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainContainer from './containers/MainContainer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <MainContainer></MainContainer>
     </div>
   );
 }
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderComponent from '../components/HeaderComponent';
+
+function MainContainer(){
+    return(
+        <HeaderComponent></HeaderComponent>
+    );
+}
+
+export default MainContainer;
