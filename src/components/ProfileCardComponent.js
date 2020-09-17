a4c744c8a0d954b0f4ec018ddd7f1c9b046bfd8a
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-09-18 02:03:31 +0530

    Add username to profile card

--- src/components/ProfileCardComponent.js
+++ src/components/ProfileCardComponent.js
@@ -3,9 +3,9 @@ import ReactMarkdown from "react-markdown";
 import styles from '../stylesheets/style.module.css';
 
 function ProfileCardComponent(props){
-
     return(
         <div className={styles.ProfileCard}>
+            <h3>{props.profileDetails.githubUserName}</h3>
             <ReactMarkdown
                 source={props.markDown}
             ></ReactMarkdown>
