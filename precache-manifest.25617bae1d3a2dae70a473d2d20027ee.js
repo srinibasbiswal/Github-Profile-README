5cab5e1f9abbe1049921e21b6674f7b27740a959
srinibasbiswal <srinibasbiswal@users.noreply.github.com>
2020-09-25 11:56:41 +0000

    deploy: 2f2889624319894eadd7b075aca30002fe9c75cb

---
+++ precache-manifest.25617bae1d3a2dae70a473d2d20027ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1bb1720c8442dc82592004f5f6eed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1fdbf6a4c7d5c11e274",
+    "url": "/static/css/main.5e9a3cd1.chunk.css"
+  },
+  {
+    "revision": "e55e5da6df2ef7475179",
+    "url": "/static/js/2.adb153ac.chunk.js"
+  },
+  {
+    "revision": "05d5bb7b37cd264eb188b9bb0ea9612c",
+    "url": "/static/js/2.adb153ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1fdbf6a4c7d5c11e274",
+    "url": "/static/js/main.1b81092c.chunk.js"
+  },
+  {
+    "revision": "12ff952159b0f6b4c5f7",
+    "url": "/static/js/runtime-main.2ce1541d.js"
+  },
+  {
+    "revision": "bbf7bd6f8992b8d9f2b88bffd6d4b82e",
+    "url": "/static/media/srinibasbiswal.bbf7bd6f.md"
+  },
+  {
+    "revision": "204ccae94f3654aac51155907079d8f7",
+    "url": "/static/media/test.204ccae9.md"
+  }
+]);
