a6fbf70b9d5bcb6f0d4f87ab4af2cb5b7fc918e3
sonilaia <sonylaia14@gmail.com>
2020-10-02 14:47:59 +0700

    add sonilaia to the users

--- src/dataSet/users.js
+++ src/dataSet/users.js
@@ -1,84 +1,82 @@
 export const users = [
-
-    {
-        name : 'Srinibas Biswal',
-        githubUserName : 'srinibasbiswal'
-    },
-    {
-        name : "Dimas Kurniawan",
-        githubUserName : "kdimas12"
-    },
-    {
-        name : 'Caroline Rodrigues',
-        githubUserName : 'caroline-rodrigues'
-    },
-    {
-        name : 'Hridayesh Sharma',
-        githubUserName : 'vyasriday'
-    },
-    {
-        name: "Lucas Guiss Gusmão",
-        githubUserName: "lucasguiss"
-    },
-    {
-        name: "Aayushi Mittal",
-        githubUserName: "Aayushi-Mittal"
-    },
-    {
-        name:  "Aniket Singh",
-        githubUserName:  "aniketsingh98571"
-    },
-    {
-        name: "Shubham Yadav",
-        githubUserName: "shubhamdsm"
-    },
-    {
-        name: "Tanut Leelaparsert",
-        githubUserName: "tanut2539"
-    },
-    {
-        name: "Konrad Linkowski",
-        githubUserName: "KonradLinkowski"
-    },
-    {
-
-        name: "Jugal Rawlani",
-        githubUserName: "tmw9"
-    },
-    {
-        name: "Dede Saepulloh",
-        githubUserName: "dedesaepulloh"
-    },
-    {
-       name: "Aryan Chawla",
-       githubUserName: "aryan-chawla"
-    },
-    {
-        name : 'Ayush Maloo',
-        githubUserName : 'ayushmaloo'
-    },
-    {
-        name : "Aniruddh Parwal",
-        githubUserName : "aniruddhparwal"
-    },
-    {
-        name : "Daniel Sabic",
-        githubUserName : "sabicd"
-    },
-    {
-        name: "Siradanai Boonyuen",
-        githubUserName: "shin-iji"
-    },
-    {
-        name: "Anshu Kumar",
-        githubUserName: "kumaranshu72"
-    
-    },
-    {
-        name: "Mike Chen",
-        githubUserName: "DriftingShadows"
-    }
-  
-   
-
-]
+	{
+		name: 'Srinibas Biswal',
+		githubUserName: 'srinibasbiswal',
+	},
+	{
+		name: 'Dimas Kurniawan',
+		githubUserName: 'kdimas12',
+	},
+	{
+		name: 'Caroline Rodrigues',
+		githubUserName: 'caroline-rodrigues',
+	},
+	{
+		name: 'Hridayesh Sharma',
+		githubUserName: 'vyasriday',
+	},
+	{
+		name: 'Lucas Guiss Gusmão',
+		githubUserName: 'lucasguiss',
+	},
+	{
+		name: 'Aayushi Mittal',
+		githubUserName: 'Aayushi-Mittal',
+	},
+	{
+		name: 'Aniket Singh',
+		githubUserName: 'aniketsingh98571',
+	},
+	{
+		name: 'Shubham Yadav',
+		githubUserName: 'shubhamdsm',
+	},
+	{
+		name: 'Tanut Leelaparsert',
+		githubUserName: 'tanut2539',
+	},
+	{
+		name: 'Konrad Linkowski',
+		githubUserName: 'KonradLinkowski',
+	},
+	{
+		name: 'Jugal Rawlani',
+		githubUserName: 'tmw9',
+	},
+	{
+		name: 'Dede Saepulloh',
+		githubUserName: 'dedesaepulloh',
+	},
+	{
+		name: 'Aryan Chawla',
+		githubUserName: 'aryan-chawla',
+	},
+	{
+		name: 'Ayush Maloo',
+		githubUserName: 'ayushmaloo',
+	},
+	{
+		name: 'Aniruddh Parwal',
+		githubUserName: 'aniruddhparwal',
+	},
+	{
+		name: 'Daniel Sabic',
+		githubUserName: 'sabicd',
+	},
+	{
+		name: 'Siradanai Boonyuen',
+		githubUserName: 'shin-iji',
+	},
+	{
+		name: 'Anshu Kumar',
+		githubUserName: 'kumaranshu72',
+	},
+	{
+		name: 'Mike Chen',
+		githubUserName: 'DriftingShadows',
+	},
+	{
+		name: 'Soni Laia',
+		githubUserName: 'sonilaia',
+	},
+];
